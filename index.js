cca8d2c728d73612fbc5bb6b060a33d2fd7fdae6
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-05-29 21:41:08 +0600

    Create product API

--- index.js
+++ index.js
@@ -22,10 +22,25 @@ app.listen(port, () => {
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.piip8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("parts-factory").collection("product");
-    // perform actions on the collection object
-    client.close();
-    console.log("DB Conccected");
-});
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('parts-factory').collection('product');
+
+        // Create an Api for get services data 
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
