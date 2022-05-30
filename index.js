68254f5fbe24393122d020b07cb6ecc9dd2d7384
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-05-30 22:14:47 +0600

    upload to heroku

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function run() {
 
         })
 
-        // Get single product by id
+        // Get single product by id 
         app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
